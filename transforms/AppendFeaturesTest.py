ba9ed09ad9c49979c2669c08ee11246ce4276bad
zygimantas <zygimantasaugunas@gmail.com>
2024-05-18 20:41:58 +0300

    UPD: Forgotten module.

---
+++ transforms/AppendFeaturesTest.py
@@ -0,0 +1,66 @@
+import torch
+import yaml
+
+from prithvi.Prithvi import MaskedAutoencoderViT
+
+
+class AppendFeaturesTest:
+    def __init__(self, feature_model_path, feature_model_checkpoint_path):
+        weights_path = feature_model_path
+        # load weights
+        if torch.cuda.is_available():
+            checkpoint = torch.load(weights_path)
+            # device = torch.device("cuda")
+            # checkpoint.to(device)
+        else:
+            checkpoint = torch.load(weights_path, map_location="cpu")
+
+        # read model config
+        model_cfg_path = feature_model_checkpoint_path
+        with open(model_cfg_path) as f:
+            model_config = yaml.safe_load(f)
+
+        model_args = model_config["model_args"]
+
+        # let us use only 1 frame for now (the model was trained on 3 frames)
+        model_args["num_frames"] = 1
+
+        # instantiate model
+        model = MaskedAutoencoderViT(**model_args)
+        if torch.cuda.is_available():
+            model.to('cuda')
+        model.eval()
+
+        # load weights into model
+        # strict=false since we are loading with only 1 frame, but the warning is expected
+        del checkpoint['pos_embed']
+        del checkpoint['decoder_pos_embed']
+        _ = model.load_state_dict(checkpoint, strict=False)
+        if torch.cuda.is_available():
+            model.to('cuda')
+        self.model = model
+
+    def __call__(self, samples):
+        img1, img2 = samples
+        # only take b,g,r,nir,swir
+        img1_reshaped = img1[:, [2, 1, 0, 3, 8, 9], :, :]
+        img2_reshaped = img2[:, [2, 1, 0, 3, 8, 9], :, :]
+        img1_reshaped = img1_reshaped.reshape(
+            *img1_reshaped.shape[:2], 1, *img1_reshaped.shape[-2:]).float()
+        img2_reshaped = img2_reshaped.reshape(
+            *img2_reshaped.shape[:2], 1, *img2_reshaped.shape[-2:]).float()
+        mask_ratio = 0.75
+        if torch.cuda.is_available():
+            img1_reshaped = img1_reshaped.cuda()
+            img2_reshaped = img2_reshaped.cuda()
+
+        with torch.no_grad():
+            features1, _, __ = self.model(img1_reshaped, mask_ratio)
+            features2, _, __ = self.model(img2_reshaped, mask_ratio)
+
+        features1 = torch.flatten(features1, 1)
+        features2 = torch.flatten(features2, 1)
+        if torch.cuda.is_available():
+            img1, img2, features1, features2 = img1.cuda(
+            ), img2.cuda(), features1.cuda(), features2.cuda()
+        return img1, img2, features1, features2
