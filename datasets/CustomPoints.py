776588af3baa85f03ef11fb0cf48fb6e670c3bc5
zygimantas <zygimantasaugunas@gmail.com>
2024-05-18 20:40:49 +0300

    ADD: Rating script.

---
+++ datasets/CustomPoints.py
@@ -0,0 +1,35 @@
+import torch
+from torch.utils.data import Dataset
+import rasterio
+
+
+class CustomPoints(Dataset):
+    def __init__(self, path, names_file, transform=None):
+        self.path = path
+        self.transform = transform
+        # read folder files
+        with open(names_file, 'r') as f:
+            self.places = f.readlines()
+            # remove whitespace characters like `\n` at the end of each line
+            self.places = [x.strip() for x in self.places]
+
+    def read_point(self, point_path):
+        src = rasterio.open(point_path)
+        return src.read().astype('float32')
+
+    def __len__(self):
+        return len(self.places) - 1
+
+    def __getitem__(self, idx):
+        point_filename = [self.places[idx], self.places[idx+1]]
+        points = [self.read_point(point_filename[0]),
+                  self.read_point(point_filename[1])]
+        # crop to square
+        min_size = min(points[0].shape[1], points[1].shape[1],
+                       points[0].shape[2], points[1].shape[2])
+        points[0] = points[0][:, :224, :224]
+        points[1] = points[1][:, :224, :224]
+        if self.transform:
+            points = self.transform(points)
+
+        return torch.from_numpy(points[0]).float(), torch.from_numpy(points[1]).float()
